1a9b68249ff573c1ca9b2f93bf6e24f8bf588670
kotteshtechboss07 <kottesh2005@gmail.com>
2024-07-24 17:01:00 +0530

    no_changes

---
+++ spread_operator.js
@@ -0,0 +1,9 @@
+// const arr1 = ["Cecilie", "Lone"];
+// const arr2 = ["Emil", "Tobias", "Linus"];
+// const children = arr1.concat(arr2); 
+// console.log(children);
+const arr = [{'name': 'kottesh'},{'name':'sham'},{'name':'maaz'}];
+// const arr1 = arr.slice(0,2);
+// console.log(arr);
+// console.log(arr1);
+
